14e7a7a810749ed5b4587f0cbd5e93afcc7092a4
SalemGrayzi <99489268+SalemGrayzi@users.noreply.github.com>
2022-02-17 19:47:38 +0200

    Update demo-uber-nyc-pickups.py

--- demo-uber-nyc-pickups.py
+++ demo-uber-nyc-pickups.py
@@ -10,7 +10,7 @@ DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
 
 @st.cache
 def load_data(nrows):
-    data = pd.read_csv("DATA_URL", nrows=nrows)
+    data = pd.read_csv(DATA_URL, nrows=nrows)
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
@@ -34,3 +34,5 @@ filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 
 st.subheader('Map of all pickups at %s:00' % hour_to_filter)
 st.map(filtered_data)
+
+streamlit run demo-uber-nyc-pickups.py
