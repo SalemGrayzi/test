361fca19933873ce5d48afd3e51021698c1b733a
SalemGrayzi <99489268+SalemGrayzi@users.noreply.github.com>
2022-02-17 19:45:38 +0200

    Update demo-uber-nyc-pickups.py

--- demo-uber-nyc-pickups.py
+++ demo-uber-nyc-pickups.py
@@ -5,12 +5,12 @@ import numpy as np
 st.title('Uber pickups in NYC')
 
 DATE_COLUMN = 'date/time'
-#DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            #'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
 @st.cache
 def load_data(nrows):
-    data = pd.read_csv("https://github.com/streamlit/demo-uber-nyc-pickups/blob/main/uber-raw-data-sep14.csv.gz", nrows=nrows)
+    data = pd.read_csv("DATA_URL", nrows=nrows)
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
